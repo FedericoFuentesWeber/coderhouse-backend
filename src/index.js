b79a2580753e31576b9fc0c64a7b93d41bd58a86
FedericoFuentesWeber <ffuentesweber@gmail.com>
2023-12-09 00:08:12 -0300

    :sparkles: Add product manager file based class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { ProductManager } from "./ProductManager/ProductManager.js";
+import { ProductManagerFileBased } from "./ProductManager/ProductManagerFileBased.js";
+import { promises as fs } from "node:fs";
 
 const execution = () => {
     const product = {
@@ -23,4 +25,60 @@ const execution = () => {
     productManager.getProductById(29);
 }
 
-execution();
+const fileBasedExecution = async() => {
+
+    const filePath = "./Products.json";
+
+    const product = {
+        title: "Test product",
+        description: "A product use for testing",
+        price: 50,
+        thumbnail: "No image",
+        code: "123",
+        stock: 10
+    };
+
+    const product2 = {
+        title: "Test product 2",
+        description: "A product use for testing",
+        price: 50,
+        thumbnail: "No image",
+        code: "1234",
+        stock: 10
+    };
+
+    const updatedProduct = {
+        title: "Test product updated",
+        description: "A product use for testing",
+        price: 50,
+        thumbnail: "No image",
+        code: "456",
+        stock: 10
+    };
+
+    const productManager = new ProductManagerFileBased(filePath);
+
+    let products = await productManager.getProducts();
+    console.log(products);
+
+    await productManager.addProduct(product);
+    await productManager.addProduct(product2);
+    products = await productManager.getProducts();
+    console.log(products);
+
+    let foundProduct = await productManager.getProductById(1);
+    console.log(foundProduct);
+
+    await productManager.updateProduct(1, updatedProduct);
+    foundProduct = await productManager.getProductById(1);
+    console.log(foundProduct);
+
+    await productManager.deleteProduct(1);
+    products = await productManager.getProducts();
+    console.log(products);
+
+    await fs.writeFile(filePath, [], "utf-8");
+}
+
+//execution();
+fileBasedExecution();
